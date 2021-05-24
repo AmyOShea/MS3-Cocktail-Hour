062e921724014a93142c466ec85e3bbc342d3158
AmyOShea <amyoshea.lsad@gmail.com>
2021-05-24 14:43:09 +0000

    Add extra ingredient input onclick

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('select').formSelect();
-    $('.tooltipped').tooltip();
-  });
+    $('#add-ingredient').on('click', function () {
+        const addIngredient = '<div class="input-field"><i class="fas fa-wine-bottle prefix"></i><input id="ingredients" name="ingredients" type="text" class="validate" required><label for="ingredients">Ingredients</label></div>'
+        $('#new-ingredient').append(addIngredient);
+    });
+});
