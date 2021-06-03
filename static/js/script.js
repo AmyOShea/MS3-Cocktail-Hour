958422a51881af37ccb4fed487c87b7a6d28c41b
AmyOShea <amyoshea.lsad@gmail.com>
2021-06-03 17:00:44 +0000

    BUG: Update form input id's and labels

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $(".dropdown-trigger").dropdown({
@@ -20,33 +22,48 @@ $(document).ready(function(){
 
     autoplay();
     function autoplay() {
-    $('.carousel').carousel('next');
-    setTimeout(autoplay, 3500);
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 3500);
     }
 
+    // Dynamically adding more ingredient inputs
+    let ing = 1;
+
+    $(".add-ingredient").click(function (e) {
+        e.preventDefault();
+            ing++;
+            $(".new-ing").append(`
+            <div class="input-field">
+            <i class="fas fa-wine-bottle prefix"></i>
+            <input id="ingredients${ing}" name="ingredients" type="text" class="validate" required>
+            <label for="ingredients${ing}">Ingredient</label>
+            <button type="button" class="btn deleteIngredient">Delete</button></div>`);
+    });
+    
+    // Dynamically removing added ingredient inputs
+    $("body").on('click', ".deleteIngredient", function () {
+        $(this).parent('div').remove();
+        ing--;
+    });
+    
+    // Method
+    let step = 1;
+    
+    // Dynamically adding more method steps inputs
+    $(".add-step").click(function (e) {
+        e.preventDefault();
+            step++;
+            $(".new-step").append(`
+            <div class="input-field">
+            <i class="fas fa-list prefix"></i>
+            <input id="method${step}" name="method" type="text" class="validate" required>
+            <label for="method${step}">Method</label>
+            <button type="button" class="btn deleteStep">Delete</button></div>`);
+    });
+
+    // Dynamically removing added method steps inputs
+    $("body").on('click', ".deleteStep", function () {
+        $(this).parent('div').remove();
+        step--;
+    });
 });
-
-// Add another ingredient input
-function addIngredient(newIng) {
-    let addIng = document.createElement('div');
-    addIng.innerHTML = '<div id="ingredientInput" class="input-field"><i class="fas fa-wine-bottle prefix"></i><input id="ingredients" type="text" name="ingredients" class="validate"><label for="ingredients" placeholder="Ingredient">Ingredient</label><a class="btn" onClick="deleteIngredient(this)">Remove</a></div>'
-    document.getElementById(newIng).appendChild(addIng);
-};
-
-//Delete added ingredient input
-function deleteIngredient(element) {
- document.getElementById("ingredientInput").remove();
-}
-
-
-// Add another method input
-function addSteps(newStep) {
-    let addStep = document.createElement('div');
-    addStep.innerHTML = '<div id="methodholder" class="input-field"><i class="fas fa-list prefix"></i><input id="method" type="text" name="method" class="validate"><label for="method">Method</label><a class="btn" onClick="deleteStep(this)">Remove</a></div>'
-    document.getElementById(newStep).appendChild(addStep);
-};
-
-//Delete added method input
-function deleteStep(element) {
- document.getElementById("methodholder").remove();
-}
