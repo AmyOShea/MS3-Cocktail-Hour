22fd3ea11a99ae93ae0082bf959ed9760b014624
AmyOShea <amyoshea.lsad@gmail.com>
2021-06-08 14:43:19 +0000

    BUG: add 'if statement for topButton

--- static/js/script.js
+++ static/js/script.js
@@ -74,23 +74,26 @@ $(document).ready(function(){
 
 
 // https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
-
 topButton = document.getElementById("toTopBtn");
 
-// When the user scrolls down 500px from the top of the document, show the button
+// functions only called if the botton exists
+if (topButton !== null) {
 
-window.onscroll = function() {scrollFunction()};
+    // When the user scrolls down 500px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    topButton.style.display = "block";
-  } else {
-    topButton.style.display = "none";
-  }
+    function scrollFunction() {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            topButton.style.display = "block";
+        } else {
+            topButton.style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
