17cffde194c6002499bb34f1be09d8784da0c431
AmyOShea <amyoshea.lsad@gmail.com>
2021-06-11 21:09:44 +0000

    REFAC: Update JS for JSHint compliance

--- static/js/script.js
+++ static/js/script.js
@@ -74,28 +74,29 @@ $(document).ready(function(){
 
 
 // https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
-topButton = document.getElementById("toTopBtn");
+const topButton = document.getElementById("toTopBtn");
 
 // functions only called if the botton exists
 if (topButton !== null) {
+    window.onscroll = function() {scrollFunction();
+    };
+}
 
-    // When the user scrolls down 500px from the top of the document, show the button
-    window.onscroll = function() {scrollFunction()};
-
-    function scrollFunction() {
-        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-            topButton.style.display = "block";
-        } else {
-            topButton.style.display = "none";
-        }
-    }
-
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// When the user scrolls down 500px from the top of the document, show the button
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
     }
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+document.body.scrollTop = 0; // For Safari
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 
 //https://www.w3schools.com/jsref/met_his_back.asp
 function goBack() {
